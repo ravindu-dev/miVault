cad30678884c539d717c4b60e31eaa0ccb53a893
ravindu-dev <rsathsara96@gmail.com>
2024-07-08 18:32:56 +0530

    Update constants.dart

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -5,10 +5,10 @@ class Constant {
 static const REFRESH_TOKEN_KEY = 'refresh_token';
 static const BACKEND_TOKEN_KEY = 'backend_token';
 static const GOOGLE_ISSUER = 'https://accounts.google.com';
-static const GOOGLE_CLIENT_ID_IOS = '26382969981-i6shbfashvvchkecbl4s88tlejr317nf.apps.googleusercontent.com';
-static const GOOGLE_REDIRECT_URI_IOS = 'com.googleusercontent.apps.26382969981-i6shbfashvvchkecbl4s88tlejr317nf.apps.googleusercontent.com:/oauthredirect';
-static const GOOGLE_CLIENT_ID_ANDROID = '26382969981-7uh9i2oncd8ajd7l1r66ft0rbf73oc3t.apps.googleusercontent.com';
-static const GOOGLE_REDIRECT_URI_ANDROID = 'com.googleusercontent.apps.26382969981-7uh9i2oncd8ajd7l1r66ft0rbf73oc3t.apps.googleusercontent.com:/oauthredirect';
+static const GOOGLE_CLIENT_ID_IOS = '<IOS-CLIENT-ID>';
+static const GOOGLE_REDIRECT_URI_IOS = 'com.googleusercontent.apps.<IOS-CLIENT-ID>.apps.googleusercontent.com:/oauthredirect';
+static const GOOGLE_CLIENT_ID_ANDROID = '<ANDROID-CLIENT-ID>';
+static const GOOGLE_REDIRECT_URI_ANDROID = 'com.googleusercontent.apps.<ANDROID-CLIENT-ID>.apps.googleusercontent.com:/oauthredirect';
 
 static String clientID() {
   if(Platform.isAndroid) {
